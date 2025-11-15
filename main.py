784bd7d4cf7cdad503fbc18d58dddba39d427722
Mikhail Shubin <heimcbk201@gmail.com>
2025-11-15 17:45:26 +0300

    Данные о пользователях теперь подгружаются из файла users.json

--- main.py
+++ main.py
@@ -3,63 +3,31 @@ import json
 import random
 import datetime
 
-with open('secrets.json', 'r') as file:
-    token = json.loads(file.read())['Santa_Bobot']
+with open("data/secrets.json", "r") as file:
+    token = json.loads(file.read())["Santa_Bobot"]
     file.close()
 
 bot = telebot.TeleBot(token)
 
-exceptions = {
-    'Михаил': ('Михаил', 'Регина'),
-    'Регина': ('Регина', 'Михаил'),
-    'Дмитрий': ('Дмитрий', 'Евгения'),
-    'Евгения': ('Евгения', 'Дмитрий'),
-    'Сергей': ('Сергей', 'Татьяна'),
-    'Татьяна': ('Татьяна', 'Сергей'),
-    'Пётр': ('Пётр', 'Ксения'),
-    'Ксения': ('Ксения', 'Пётр'),
-    'Борис': ('Борис', 'Анастасия'),
-    'Анастасия': ('Анастасия', 'Борис')
-}
+with open("data/users.json", "r", encoding="UTF-8") as file:
+    exceptions = json.loads(file.read())["exceptions"]
+    file.close()
+print(exceptions)
 
-invited_users = {
-    'mishgun64': 'Михаил',
-    'regialf': 'Регина',
-    'Qenter': 'Дмитрий',
-    'in_genue': 'Евгения',
-    'oOSRGOo': 'Сергей',
-    'Rrikki_tikki_tavi': 'Татьяна',
-    'creatoff': 'Пётр',
-    'sad_sad_morning': 'Ксения',
-    'arhitectbg': 'Борис',
-    'nanmoore': 'Анастасия'
-}
+with open("data/users.json", "r", encoding="UTF-8") as file:
+    invited_users = json.loads(file.read())["invited_users"]
+    file.close()
+print(invited_users)
 
-bans = {
-    'Михаил': '',
-    'Регина': '',
-    'Дмитрий': '',
-    'Евгения': '',
-    'Сергей': '',
-    'Татьяна': '',
-    'Пётр': '',
-    'Ксения': '',
-    'Борис': '',
-    'Анастасия': ''
-}
+with open("data/users.json", "r", encoding="UTF-8") as file:
+    bans = json.loads(file.read())["bans"]
+    file.close()
+print(bans)
 
-is_registred = {
-    'Михаил': '❌',
-    'Регина': '❌',
-    'Дмитрий': '❌',
-    'Евгения': '❌',
-    'Сергей': '❌',
-    'Татьяна': '❌',
-    'Пётр': '❌',
-    'Ксения': '❌',
-    'Борис': '❌',
-    'Анастасия': '❌'
-}
+with open("data/users.json", "r", encoding="UTF-8") as file:
+    is_registred = json.loads(file.read())["is_registred"]
+    file.close()
+print(is_registred)
 
 commands = [
     telebot.types.BotCommand("start", "Запустить бота"),
@@ -67,28 +35,28 @@ commands = [
 bot.set_my_commands(commands)
 def logger(text):
     try:
-        with open('./data/log', mode='a', encoding='UTF-8') as log:
-            log.write(str(datetime.datetime.now()) + ' - ' + text + '\n')
+        with open("./data/log", mode="a", encoding="UTF-8") as log:
+            log.write(str(datetime.datetime.now()) + " - " + text + "\n")
             log.close()
     except:
-        print(str(datetime.datetime.now()) + ' Логер сломался')
+        print(str(datetime.datetime.now()) + " Логер сломался")
 
 def get_registered_users():
     try:
-        with open('./data/db.json', mode='r', encoding='UTF-8') as db:
+        with open("./data/db.json", mode="r", encoding="UTF-8") as db:
             users = json.loads(db.read())
             db.close()
         return users
     except:
-        logger('Не удалось выполнить чтение из БД.')
+        logger("Не удалось выполнить чтение из БД.")
 
 def write_registered_user(users):
     try:
-        with open('./data/db.json', mode='w', encoding='UTF-8') as db:
+        with open("./data/db.json", mode="w", encoding="UTF-8") as db:
             db.write(json.dumps(users, ensure_ascii=True))
             db.close()
     except:
-        logger('Не удалось выполнить запись в БД.')
+        logger("Не удалось выполнить запись в БД.")
 
 def get_choice_range(user):
     choice_range = []
@@ -113,13 +81,13 @@ def mixer():
                     if v == dct[user]:
                         invited_users.pop(k)
                         break
-            logger('Пользователи перемешаны')
+            logger("Пользователи перемешаны")
             return dct
         except:
-            logger('Не удалось перемешать пользователей')
+            logger("Не удалось перемешать пользователей")
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
     # создаём клавиатуру
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
@@ -150,23 +118,23 @@ def handle_reg_button(message):
             button_8 = telebot.types.InlineKeyboardButton(text=ban_range[7], callback_data=invited_users[username] + ":" + ban_range[7])
             button_9 = telebot.types.InlineKeyboardButton(text="Могу дарить кому угодно", callback_data=invited_users[username] + ":" + "")
             keyboard.add(button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9)
-            bot.send_message(message.chat.id, f'Приветствую {invited_users[username]}. Выберите одного человека, которому вы не хотите дарить подарок в этом году', reply_markup=keyboard)
+            bot.send_message(message.chat.id, f"Приветствую {invited_users[username]}. Выберите одного человека, которому вы не хотите дарить подарок в этом году", reply_markup=keyboard)
         elif username in invited_users and invited_users[username] in registered_users:
-            bot.send_message(message.from_user.id, f'Вы уже зарегестирированы.')
+            bot.send_message(message.from_user.id, f"Вы уже зарегестирированы.")
         else:
-            bot.send_message(message.from_user.id, f'Вас сюда не звали {message.from_user.first_name}')
+            bot.send_message(message.from_user.id, f"Вас сюда не звали {message.from_user.first_name}")
     except:
-        logger(f'Не удалось вывести клавиатуру для пользователя {username}')
+        logger(f"Не удалось вывести клавиатуру для пользователя {username}")
 
 @bot.message_handler(func=lambda m: m.text == "Показать зарегестрированных пользователей")
 def handle_reg_users_button(message):
     registered_users = get_registered_users()
     for user, value in is_registred.items():
         if user in registered_users.keys():
-            is_registred[user] = '✅'
-    registred = ''
+            is_registred[user] = "✅"
+    registred = ""
     for k, v in is_registred.items():
-        registred += f'{v} {k}\n'
+        registred += f"{v} {k}\n"
     bot.send_message(message.chat.id, registred)
 
 
@@ -176,37 +144,37 @@ def callback_query(call: telebot.types.CallbackQuery):
     username = call.from_user.username
     bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
     try:
-        bans[data.split(':')[0]] = data.split(':')[1]
-        logger(f'Добавлен бан для {username}')
+        bans[data.split(":")[0]] = data.split(":")[1]
+        logger(f"Добавлен бан для {username}")
     except:
-        logger(f'Не удалось добавить бан для {username}')
+        logger(f"Не удалось добавить бан для {username}")
     try:
         registered_users = get_registered_users()
         id = call.from_user.id
         registered_users[invited_users[username]] = id
         write_registered_user(registered_users)
-        logger(f'Пользователь {username} зарегистрирован')
-        bot.send_message(call.from_user.id, f'Вы зарегистирированы.')
+        logger(f"Пользователь {username} зарегистрирован")
+        bot.send_message(call.from_user.id, f"Вы зарегистирированы.")
         if len(registered_users) == len(invited_users):
-            logger('Все пользователи зарегистрированы')
+            logger("Все пользователи зарегистрированы")
             mix_result = mixer()
             for name, id in registered_users.items():
                 try:
-                    message_text = f'{mix_result[name]} это тот кому вы дарите подарок'
+                    message_text = f"{mix_result[name]} это тот кому вы дарите подарок"
                     bot.send_message(id, message_text)
-                    logger(f'Пользователь {name} получил результат')
+                    logger(f"Пользователь {name} получил результат")
                 except:
-                    logger(f'Не удалось отправить сообщение с результатом пользователю {name}')
+                    logger(f"Не удалось отправить сообщение с результатом пользователю {name}")
         else:
-            bot.send_message(call.from_user.id, f'Всего зарегистрировалось {len(registered_users)}/{len(invited_users)} человек. Вы узнаете результат как только все пользователи пройдут регистрацию')
+            bot.send_message(call.from_user.id, f"Всего зарегистрировалось {len(registered_users)}/{len(invited_users)} человек. Вы узнаете результат как только все пользователи пройдут регистрацию")
     except:
-        logger(f'Не удалось зарегистрировать пользователя {username}')
+        logger(f"Не удалось зарегистрировать пользователя {username}")
 
-logger('-------------------------------------------------------------------------')
-logger('Бот запущен')
+logger("-------------------------------------------------------------------------")
+logger("Бот запущен")
 
 try:
-    open('/data/db.json', mode='r', encoding='UTF-8')
+    open("/data/db.json", mode="r", encoding="UTF-8")
 except:
     write_registered_user({})
 
